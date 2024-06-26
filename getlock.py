419d133fd013de1c97c3f597bad0f253e9949d4f
Dheeraj <dheeru2605@gmail.com>
2024-06-26 18:21:24 -0500

    Working on stock signal function

--- getlock.py
+++ getlock.py
@@ -1,10 +1,18 @@
+# GetLock trading bot
+
+# If you have a robinhood account you can use 'robin-stocks' package 
+# to interact with the Robinhood API
+
+# Link: https://pypi.org/project/robin-stocks/
+
 import yfinance as yf  # historical market data library 
 import pandas as pd 
 
-
 # Fetch historical data
 tech_stocks = ['AAPL', 'MSFT', 'NVDA', 'GOOGL']
-data = yf.download(tech_stocks, start='2024-06-01', end='2024-06-25')
+data = yf.download(tech_stocks, start='2024-06-01', end='2024-06-25', interval='15m')
 
-# Print the first few rows of the data
-print(data.head())
+# Making a signal function to detect bearish/bullish patterns in the market 
+
+def stock_signal(df):
+    pass
